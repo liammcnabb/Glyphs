5b2408a61a71a70c95f1d3f80624e2d59207622f
liam mcnabb <liammcnabb@gmail.com>
2018-07-23 15:27:33 +0100

    Initial piechart variables

--- Application/piechart.cpp
+++ Application/piechart.cpp
@@ -1,12 +1,32 @@
 #include "piechart.h"
 
-PieChart::PieChart(QPointF centroid) : Glyph( centroid )
+PieChart::PieChart(QPointF centroid, float size) : Glyph( centroid )
 {
-
+    setSize( size );
 }
 
 void PieChart::initialize(QStringList values)
 {
-
+    int slices = values.size();
     return;
+}
+
+float PieChart::size() const
+{
+    return m_size;
+}
+
+void PieChart::setSize(float size)
+{
+    m_size = size;
+}
+
+float PieSegment::angle() const
+{
+    return m_angle;
+}
+
+void PieSegment::setAngle(float angle)
+{
+    m_angle = angle;
 }
--- Application/piechart.h
+++ Application/piechart.h
@@ -9,8 +9,23 @@
 class PieChart : public Glyph
 {
 public:
-    PieChart(QPointF centroid);
+    PieChart(QPointF centroid, float size);
     void initialize(QStringList values);
+    float size() const;
+    void setSize(float size);
+
+private:
+    float m_size = 0.0f;
+};
+
+class PieSegment
+{
+
+public:
+    float angle() const;
+    void setAngle(float angle);
+private:
+    float m_angle = 1.0f;
 };
 
 #endif // PIECHART_H
