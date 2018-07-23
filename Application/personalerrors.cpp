f0569f777ea97e5643e8c0b9e4fa0248f1ccc4f3
liam mcnabb <liammcnabb@gmail.com>
2018-07-23 14:26:57 +0100

    Created cpp file to hold Error Messages

---
+++ Application/personalerrors.cpp
@@ -0,0 +1,6 @@
+#include <QString>
+
+static const QString E = "Error";
+static const QString MISSING_MAP = "001: No Map Loaded.";
+static const QString NO_LINKED_DATA = "002: No Data linking found.";
+
