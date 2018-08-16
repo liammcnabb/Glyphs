d24a1c91c47605aaa5e5b5522d64205306aadbff
liam mcnabb <liammcnabb@gmail.com>
2018-08-16 16:08:28 +0100

    Added StarGlyph class

---
+++ Application/starglyph.cpp
@@ -0,0 +1,6 @@
+#include "starglyph.h"
+
+StarGlyph::StarGlyph()
+{
+
+}
---
+++ Application/starglyph.h
@@ -0,0 +1,11 @@
+#ifndef STARGLYPH_H
+#define STARGLYPH_H
+
+
+class StarGlyph : public Glyph
+{
+public:
+    StarGlyph();
+};
+
+#endif // STARGLYPH_H
